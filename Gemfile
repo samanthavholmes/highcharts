3f0671d7d544895d79f93659037f8fe9fbb82dd3
Samantha Holmes <samanthavholmes@gmail.com>
2016-08-23 15:33:56 -0400

    SQL queries for past 5 days and area-range graph

--- Gemfile
+++ Gemfile
@@ -4,13 +4,14 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
+gem 'materialize-sass'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
@@ -20,7 +21,12 @@ gem 'jquery-rails'
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
+gem 'mysql'
+gem 'mysql2'
+gem 'pry'
+gem 'highcharts-rails'
 gem 'lazy_high_charts'
+gem 'net-ssh-gateway', '~> 1.2'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,39 +1,95 @@
 module ChartsHelper
 
-  def create_chart
-    chart = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(text: "Population vs GDP For 5 Big Countries [2009]")
-      f.xAxis(categories: ["United States", "Japan", "China", "Germany", "France"])
-      f.series(name: "GDP in Billions", yAxis: 0, data: [14119, 5068, 4985, 3339, 2656])
-      f.series(name: "Population in Millions", yAxis: 1, data: [310, 127, 1340, 81, 65])
-
-      f.yAxis [
-        {title: {text: "GDP in Billions", margin: 70} },
-        {title: {text: "Population in Millions"}, opposite: true},
-      ]
-
-      f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
-      f.chart({defaultSeriesType: "column"})
+  def get_check_totals_by_day(day_of_week)
+    totals = {}
+    half_hour_totals = []
+    most_recent_half_hour_totals = []
+    most_recent_totals = {}
+    indexes_of_day = {sunday: 1, monday: 2, tuesday: 3, wednesday: 4, thursday: 5, friday: 6, saturday: 7}
+    # index = indexes_of_day[day_of_week.downcase.to_sym]
+    # date = Date.today.beginning_of_week(day)
+    date = Date.new(2016, 7, 10)
+    most_recent_date = {year: date.year, month: date.month, day: date.day}
+    days = [date, date -= 7, date -= 7, date -= 7, date -= 7].map! {|date| date.strftime('%m%d')}
+    hour = 17
+    lower_min = 0
+    higher_min = 29
+    until hour == 23 && higher_min == 29
+      $db.query(("SELECT total FROM ticket WHERE DATE_FORMAT(closed_at, '%m%d') IN (#{days[0]}, #{days[1]}, #{days[2]}, #{days[3]}, #{days[4]}) AND HOUR(closed_at) = #{hour} AND MINUTE(closed_at) BETWEEN #{lower_min} AND #{higher_min}")).each do |total|
+        half_hour_totals << total.values.first unless total.values.first == 0
+      end
+      $db.query(("SELECT total FROM ticket WHERE DATE_FORMAT(closed_at, '%m%d') = #{days[0]} AND HOUR(closed_at) = #{hour} AND MINUTE(closed_at) BETWEEN #{lower_min} AND #{higher_min}")).each do |total|
+        most_recent_half_hour_totals << total.values.first unless total.values.first == 0
+      end
+      utc_time = DateTime.new(most_recent_date[:year], most_recent_date[:month], most_recent_date[:day], hour, lower_min).to_i * 1000
+      totals[utc_time] = half_hour_totals
+      most_recent_totals[utc_time] = most_recent_half_hour_totals
+      half_hour_totals = []
+      most_recent_half_hour_totals = []
+      hour += 1 if higher_min == 59
+      if lower_min == 30
+        lower_min = 0
+        higher_min = 29
+      else
+        lower_min = 30
+        higher_min = 59
+      end
     end
+    return {totals: totals, most_recent_totals: most_recent_totals}
+  end
 
-    @chart_globals = LazyHighCharts::HighChartGlobals.new do |f|
-      f.global(useUTC: false)
-      f.chart(
-        backgroundColor: {
-          linearGradient: [0, 0, 500, 500],
-          stops: [
-            [0, "rgb(255, 255, 255)"],
-            [1, "rgb(240, 240, 255)"]
-          ]
-        },
-        borderWidth: 2,
-        plotBackgroundColor: "rgba(255, 255, 255, .9)",
-        plotShadow: true,
-        plotBorderWidth: 1
-      )
-      f.lang(thousandsSep: ",")
-      f.colors(["#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354"])
+  def get_ranges_by_day(totals)
+    totals = take_off_highest_lowest(totals)
+    ten_percent_of_totals_length = totals.length * 1/10
+    min = get_median(totals[0...ten_percent_of_totals_length], ten_percent_of_totals_length).round(2)
+    max = get_median(totals[-ten_percent_of_totals_length..-1], ten_percent_of_totals_length).round(2)
+    return [min, max]
+  end
+
+  def get_median(totals, ten_percent_of_totals_length)
+    if totals.length.odd?
+      return totals[ten_percent_of_totals_length/2]
+    else
+      return totals[(ten_percent_of_totals_length/2) -1.. ten_percent_of_totals_length/2].reduce(:+)/2
     end
-  return chart
+  end
+
+  def take_off_highest_lowest(totals)
+    totals.map! {|total| total/100.00}
+    totals.shift
+    totals.pop
+    return totals
+  end
+
+  def get_avg_by_day(totals)
+    totals = take_off_highest_lowest(totals) unless totals.length <= 2
+    return [(totals.reduce(:+)/totals.length).round(2)]
+  end
+
+  def get_ranges_for_week(totals)
+    final_totals = []
+    totals.each do |time, hour_totals|
+      next if hour_totals.empty?
+      hour_totals.sort!.uniq!
+      final_totals << [time] + get_ranges_by_day(hour_totals)
+    end
+    return final_totals
+  end
+
+  def get_avg_for_most_recent(totals)
+    final_totals = []
+    totals.each do |time, hour_totals|
+      next if hour_totals.length <= 1
+      hour_totals.sort!.uniq!
+      final_totals << [time] + get_avg_by_day(hour_totals)
+    end
+    return final_totals
+  end
+
+  def get_max_of_all_totals(ranges, most_recent_totals)
+    all_totals = []
+    ranges.each {|total| all_totals << total[2]}
+    # most_recent_totals.each {|total| all_totals << total[1]}
+    return all_totals.max
   end
 end
